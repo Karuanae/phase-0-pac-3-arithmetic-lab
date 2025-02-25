47f3d5f3c54932c355ab1a1e26be54cdb3e6e7ba
karuana <emilykaruanamwangi@gmail.com>
2025-02-26 01:44:33 +0300

    Submit assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+1 + 80; //=> 81
+}
+function subtract(a, b) {
+60 - 40; //=> 20
+}
+function multiply(a, b) {
+2 * 3.4; //=> 6.8
+}
+function divide(a, b) {
+5.0 / 2.5; //=> 2
+}
+
+function add(a, b) {
+    return a + b;
+  }
+
+    function subtract(a, b) {
+        return a - b;
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    function divide(a, b) {
+        return a / b;
+    }
+
+function increment(n) {
+    return n += 1;
+
+}
+
+function decrement(n) {
+    return n -= 1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
